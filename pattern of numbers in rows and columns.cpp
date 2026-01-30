d0bf804780bb3812755d0396fe3e8dfbb2a7ae36
Kslohar <159747157+Kslohar@users.noreply.github.com>
2026-01-30 21:38:26 +0530

    Add files via upload

---
+++ pattern of numbers in rows and columns.cpp
@@ -0,0 +1,20 @@
+//star pattern with number of rows and coloums
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int m;
+    cout<<"Enter the number of rows:";
+    cin>>n;
+    cout<<"Enter the number of columns:";
+    cin>>m;
+    for(int i=1;i<=n;i++){
+    
+        for(int j=1;j<=m;j++){
+           
+            cout<<"*";
+        }
+       cout<<endl;
+}
+return 0;
+}
